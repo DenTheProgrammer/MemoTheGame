2ed6f285a185c05b8055e8504b16ca21306f199f
DenTheProgrammer <den4ikk007@gmail.com>
2018-09-16 21:41:47 +0300

    assosiated cards check

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var game = {
     attempts: 0,
     cards: 4,
-    flipped: 0,
+    flipped: [],
+    assosiatedCards: [2, 3, 0, 1],
 }
 
 function cardAppend() {
@@ -14,19 +15,38 @@ function cardAppend() {
         card.classList.add("card");
         cardBack.classList.add("cardBack");
         cardFront.classList.add("cardFront");
+        cardFront.classList.add("cardFront" + i); //индекс карточки
         card.appendChild(cardBack);
         card.appendChild(cardFront);
         container.appendChild(card);
     }
 }
 
+function unflip() {
+    document.querySelector(".cardFront" + game.flipped[0]).parentNode.classList.toggle("flip");
+    document.querySelector(".cardFront" + game.flipped[1]).parentNode.classList.toggle("flip");
+    game.flipped = [];
+}
+
 document.querySelector(".gameContainer").addEventListener("click", function (e) {
-    if (e.target.classList[0] === "cardFront"){
+    if (e.target.classList[0] === "cardFront") {
         e.target.parentNode.classList.toggle("flip");
         game.attempts++;
+        game.flipped.push(+e.target.classList[1][9]);
+        if (game.flipped.length === 2) {
+            if (game.assosiatedCards[game.flipped[0]] === game.flipped[1]) {
+                console.log("совпадение");
+                game.flipped = [];
+            } else {
+                setTimeout(unflip, 2000);
+            }
+
+
+
+        }
     }
 
-      
+
 })
 
 
